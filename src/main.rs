242d74b467b3333ea51da7954d32f99f6f387120
Mathilde Grapin <mathilde@fkotd.me>
2021-08-07 23:22:26 +0200

    Remove tilemap plugin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use bevy::{input::system::exit_on_esc_system, prelude::*};
-use bevy_tiled_prototype::TiledMapCenter;
 
 struct Character;
 
@@ -12,7 +11,6 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
-        .add_plugin(bevy_tiled_prototype::TiledMapPlugin)
         .add_startup_system(setup.system())
         .add_system(exit_on_esc_system.system())
         .add_system(character_movement.system())
@@ -22,25 +20,46 @@ fn main() {
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let character_texture_handle = asset_server.load("sprites/persono.png");
-    let map_handle = asset_server.load("maps/ground.tmx");
+    let ground_texture_handle = asset_server.load("maps/tile.png");
 
-    commands
-        .spawn_bundle(bevy_tiled_prototype::TiledMapBundle {
-            map_asset: map_handle,
-            center: TiledMapCenter(true),
-            ..Default::default()
-        });
+    let width = 12;
+    let height = 16;
+    let tile_size = 32;
+    let tile_half_size = tile_size / 2;
+    let tile_quarter_size = tile_size / 4;
+
+    for i in 0..width {
+        for j in 0..height {
+            commands
+                .spawn_bundle(SpriteBundle {
+                    material: materials.add(ground_texture_handle.clone().into()),
+                    transform: Transform::from_translation(Vec3::new(
+                            ((i * tile_half_size) + (-j * tile_half_size)) as f32,
+                            ((-i * tile_quarter_size) + (-j * tile_quarter_size)) as f32,
+                            0.
+                    )),
+                    ..Default::default()
+                });
+        }
+    }
+    
     commands
         .spawn_bundle(SpriteBundle {
-        material: materials.add(character_texture_handle.into()),
-        transform: Transform::from_translation(Vec3::new(0., 0., 2.)),
-        ..Default::default()
+            material: materials.add(character_texture_handle.into()),
+            transform: Transform::from_translation(Vec3::new(
+                    0.,
+                    0.,
+                    0.,
+            )),
+            ..Default::default()
         })
         .insert(Character);
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+
 }
 
 fn character_movement(
@@ -49,16 +68,16 @@ fn character_movement(
 ) {
     for mut transfrom in character_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            transfrom.translation.x -=1.;
+            transfrom.translation.x -=2.;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transfrom.translation.x +=1.;
+            transfrom.translation.x +=2.;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transfrom.translation.y -=1.;
+            transfrom.translation.y -=2.;
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            transfrom.translation.y +=1.;
+            transfrom.translation.y +=2.;
         }
     } 
 }
