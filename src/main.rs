9ee4c72833eabcd9b9703eaf6b51271e400ee731
Mathilde Grapin <mathilde@fkotd.me>
2021-09-16 21:19:10 +0200

    Add isometric parabola

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use bevy::{input::system::exit_on_esc_system, prelude::*};
 
 const WINDOW_WIDTH: f32 = 640.;
 const WINDOW_HEIGHT: f32 = 480.;
-const COURT_WIDTH: i32 = 5;
-const COURT_HEIGHT: i32 = 9;
-const COURT_X_TRANSLATION: i32 = 2;
-const TILE_SIZE: i32 = 32;
+// const COURT_WIDTH: i32 = 5;
+// const COURT_HEIGHT: i32 = 9;
+// const COURT_X_TRANSLATION: i32 = 2;
+// const TILE_SIZE: i32 = 32;
 
 struct Character {
     speed: f32,
@@ -13,9 +13,13 @@ struct Character {
 
 struct Projectile {
     velocity: Vec2,
+    start_height: f32,
 }
 
 struct Gravity(f32);
+struct CartesianTransform {
+    transform: Transform,
+}
 
 struct ThrowBallEvent();
 
@@ -32,8 +36,11 @@ fn main() {
         .add_event::<ThrowBallEvent>()
         .add_system(exit_on_esc_system.system())
         .add_system(throw_ball.system())
-        .add_system(character_movement.system())
-        .add_system(ball_movement.system())
+        .add_system(move_character.system())
+        .add_system(move_ball.system())
+        // .add_system(ball_movement.system())
+        .add_system(cartesian_to_iso.system())
+        .add_system(ball_cartesian_to_iso.system())
         .run();
 }
 
@@ -44,20 +51,41 @@ fn setup(
 ) {
     let character_a_texture_handle = asset_server.load("sprites/character_a.png");
     let character_b_texture_handle = asset_server.load("sprites/character_b.png");
-    let ground_texture_handle = asset_server.load("maps/ground_top.png");
+    // let ground_texture_handle = asset_server.load("maps/ground_top.png");
+    let ground_texture_handle = asset_server.load("maps/tile.png");
 
     // create court
-    let court_height = COURT_HEIGHT * TILE_SIZE;
-    let court_width = COURT_WIDTH * TILE_SIZE;
-    let court_x_translation = COURT_X_TRANSLATION * TILE_SIZE;
+    // top down view
+    // let court_height = COURT_HEIGHT * TILE_SIZE;
+    // let court_width = COURT_WIDTH * TILE_SIZE;
+    // let court_x_translation = COURT_X_TRANSLATION * TILE_SIZE;
 
-    for i in 0..COURT_WIDTH {
-        for j in 0..COURT_HEIGHT {
+    // for i in 0..COURT_WIDTH {
+    // for j in 0..COURT_HEIGHT {
+    // commands.spawn_bundle(SpriteBundle {
+    // material: materials.add(ground_texture_handle.clone().into()),
+    // transform: Transform::from_translation(Vec3::new(
+    // ((i * TILE_SIZE) - (court_width / 2) - court_x_translation) as f32,
+    // ((-j * TILE_SIZE) + (court_height / 2)) as f32,
+    // 0.,
+    // )),
+    // ..Default::default()
+    // });
+    // }
+    // }
+
+    // isomtric view
+    let court_width = 12;
+    let court_height = 16;
+    let tile_size = 32;
+
+    for i in 0..court_width {
+        for j in 0..court_height {
             commands.spawn_bundle(SpriteBundle {
                 material: materials.add(ground_texture_handle.clone().into()),
                 transform: Transform::from_translation(Vec3::new(
-                    ((i * TILE_SIZE) - (court_width / 2) - court_x_translation) as f32,
-                    ((-j * TILE_SIZE) + (court_height / 2)) as f32,
+                    ((i * (tile_size / 2)) + (-j * (tile_size / 2))) as f32,
+                    ((-i * (tile_size / 4)) + (-j * (tile_size / 4))) as f32,
                     0.,
                 )),
                 ..Default::default()
@@ -69,52 +97,49 @@ fn setup(
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(character_a_texture_handle.into()),
-            transform: Transform::from_translation(Vec3::new(
-                (-court_x_translation) as f32,
-                (-court_height / 2 + TILE_SIZE) as f32,
-                0.,
-            )),
             ..Default::default()
+        })
+        .insert(CartesianTransform {
+            transform: Transform::from_translation(Vec3::new(110., -205., 0.)),
         })
         .insert(Character { speed: 150. });
 
     // create opponent character
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.add(character_b_texture_handle.into()),
-        transform: Transform::from_translation(Vec3::new(
-            (-court_x_translation - TILE_SIZE) as f32,
-            (court_height / 2) as f32,
-            0.,
-        )),
-        ..Default::default()
-    });
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(character_b_texture_handle.into()),
+            ..Default::default()
+        })
+        .insert(CartesianTransform {
+            transform: Transform::from_translation(Vec3::new(40., 0., 0.)),
+        });
 
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
-fn character_movement(
+fn move_character(
     keyboard_input: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut character_positions: Query<(&mut Transform, &Character)>,
+    mut character_positions: Query<(&mut CartesianTransform, &Character)>,
     mut throw_ball_event: EventReader<ThrowBallEvent>,
 ) {
-    for event in throw_ball_event.iter() {
-        eprintln!("We are throwing a ball!");
+    for _event in throw_ball_event.iter() {
+        // eprintln!("We are throwing a ball!");
         return;
     }
 
-    for (mut transform, character) in character_positions.iter_mut() {
+    for (mut cartesian, character) in character_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= character.speed * time.delta_seconds();
+            cartesian.transform.translation.x -= character.speed * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += character.speed * time.delta_seconds();
+            cartesian.transform.translation.x += character.speed * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= character.speed * time.delta_seconds();
+            cartesian.transform.translation.y -= character.speed * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += character.speed * time.delta_seconds();
+            cartesian.transform.translation.y += character.speed * time.delta_seconds();
         }
     }
 }
@@ -123,7 +148,7 @@ fn throw_ball(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    character_position: Query<&Transform, With<Character>>,
+    character_position: Query<(&Transform, &CartesianTransform), With<Character>>,
     keyboard_input: Res<Input<KeyCode>>,
     mut throw_ball_event: EventWriter<ThrowBallEvent>,
 ) {
@@ -132,14 +157,14 @@ fn throw_ball(
     }
 
     if keyboard_input.just_released(KeyCode::E) {
-        for transform in character_position.iter() {
+        for (transform, cartesian) in character_position.iter() {
             let ball_texture_handle = asset_server.load("sprites/ball.png");
 
             // TODO: get from player input
-            let parabola_half_distance = 60.;
-            let parabola_height = 40.;
+            let parabola_half_distance = 70.;
+            let parabola_height = 20.;
 
-            let initial_velocity_x = 100.;
+            let initial_velocity_x = 200.;
             let initial_velocity_y =
                 (2. * parabola_height * initial_velocity_x) / parabola_half_distance;
 
@@ -154,19 +179,76 @@ fn throw_ball(
                 })
                 .insert(Projectile {
                     velocity: Vec2::new(initial_velocity_x, initial_velocity_y),
+                    start_height: cartesian.transform.clone().translation.y,
+                })
+                .insert(CartesianTransform {
+                    transform: cartesian.transform.clone(),
                 })
                 .insert(Gravity(gravity));
         }
     }
 }
 
-fn ball_movement(time: Res<Time>, mut ball: Query<(&mut Transform, &mut Projectile, &Gravity)>) {
-    for (mut transform, mut projectile, gravity) in ball.iter_mut() {
+fn move_ball(
+    time: Res<Time>,
+    mut ball: Query<(&mut CartesianTransform, &mut Projectile, &Gravity)>,
+) {
+    for (mut cartesian, mut projectile, gravity) in ball.iter_mut() {
         let delta = time.delta_seconds();
 
-        transform.translation.x += projectile.velocity.x * delta + (gravity.0 * delta * delta / 2.);
-        transform.translation.y += projectile.velocity.y * delta + (gravity.0 * delta * delta / 2.);
+        if cartesian.transform.translation.y < projectile.start_height {
+            continue;
+        }
 
-        projectile.velocity.y += gravity.0 * delta;
+        // store the value of the height in z instead of x
+        cartesian.transform.translation.z +=
+            projectile.velocity.x * delta + (gravity.0 * delta * delta / 2.);
+        cartesian.transform.translation.y +=
+            projectile.velocity.y * delta + (gravity.0 * delta * delta / 2.);
+
+        projectile.velocity.x += gravity.0 * delta;
+    }
+}
+
+// fn ball_movement(
+// time: Res<Time>,
+// mut ball: Query<(&mut CartesianTransform, &mut Projectile, &Gravity)>,
+// ) {
+// for (mut cartesian, mut projectile, gravity) in ball.iter_mut() {
+// let delta = time.delta_seconds();
+
+// if cartesian.transform.translation.y < projectile.start_height {
+// continue;
+// }
+
+// cartesian.transform.translation.x +=
+// projectile.velocity.x * delta + (gravity.0 * delta * delta / 2.);
+// cartesian.transform.translation.y +=
+// projectile.velocity.y * delta + (gravity.0 * delta * delta / 2.);
+
+// projectile.velocity.y += gravity.0 * delta;
+// }
+// }
+
+fn cartesian_to_iso(mut ball: Query<(&mut Transform, &CartesianTransform), Without<Projectile>>) {
+    for (mut transform, cartesian) in ball.iter_mut() {
+        transform.translation.x =
+            cartesian.transform.translation.x + cartesian.transform.translation.y;
+        transform.translation.y =
+            (cartesian.transform.translation.y - cartesian.transform.translation.x) / 2.;
+    }
+}
+
+fn ball_cartesian_to_iso(mut ball: Query<(&mut Transform, &CartesianTransform), With<Projectile>>) {
+    for (mut transform, cartesian) in ball.iter_mut() {
+        transform.translation.x =
+            cartesian.transform.translation.x + cartesian.transform.translation.y;
+        transform.translation.y =
+            (cartesian.transform.translation.y - cartesian.transform.translation.x) / 2.;
+
+        // TODO: cleaner way to do that?
+        transform.translation.y += cartesian.transform.translation.z.sqrt()
+            * cartesian.transform.translation.z.sqrt()
+            - 10.;
     }
 }
